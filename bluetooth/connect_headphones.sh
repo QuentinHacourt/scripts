ca8afef8fad151ff42991aaefba4f67408af2282
QuentinHacourt <quentin.hacourt@student.ucll.be>
2020-10-26 17:37:43 +0100

    Corrected mistake in connect_headphones.sh script
    Because:
    - The script wouldn't work if bluetooth wasn't turned on before

--- bluetooth/connect_headphones.sh
+++ bluetooth/connect_headphones.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 
 coproc bluetoothctl
-echo -e 'agent on\nconnect B8:F6:53:FD:D9:7C\nexit' >&p
+echo -e 'power on\nagent on\ndefault-agent\nscan on\nconnect B8:F6:53:FD:D9:7C\nexit' >&p
 output=$(cat <&p)
 echo $output
